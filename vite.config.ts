94c5c24a0c57b4d0d7bd2ee04bbfffdb28a2d35b
Martín Rodriguez <121552411+martdrigz@users.noreply.github.com>
2026-05-27 11:50:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/LigaDV/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
