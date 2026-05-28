0724c5cda61b8c535a29b39e826af5344d12fbca
Martín Rodriguez <121552411+martdrigz@users.noreply.github.com>
2026-05-28 09:39:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/LigaDV/',
+    base: 'https://martdrigz.github.io/LigaDV/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
